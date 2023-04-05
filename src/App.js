3b62a9322aa6647ee9a0dda3cfa2ce5c6306e478
chandanch <chandan.cec107@gmail.com>
2023-04-05 21:24:58 +0530

    code cleanup and use of mui btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <h1>React MUI Primer</h1>
+      <Button variant="contained" size="small">
+        Show Details
+      </Button>
     </div>
   );
 }
