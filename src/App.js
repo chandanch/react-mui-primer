4e1ddaa4d84a7218922a83db3e3f7a3c261d7e09
chandanch <chandan.cec107@gmail.com>
2023-04-06 22:55:15 +0530

    basic use of custom mui theme :bulb:

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <Typography variant="h2">React Materail UI Primer</Typography>
-      <Button variant="contained" size="small">
+      <Button variant="contained" size="small" color="primary">
         Show User Details
       </Button>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#388E3C',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
